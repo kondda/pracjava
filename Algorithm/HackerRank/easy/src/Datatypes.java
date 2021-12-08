404922dde6a7277ad79272c26ed0818943293cf1
kondda <konddalee@gmail.com>
2021-12-08 21:45:17 +0900

    -9223372036854775808 이숫자 넣으면 long이 안나옴 왜지..

--- Algorithm/HackerRank/easy/src/Datatypes.java
+++ Algorithm/HackerRank/easy/src/Datatypes.java
@@ -9,18 +9,14 @@ public class Datatypes {
             try {
                 long x = sc.nextLong();
                 System.out.println(x+" can be fitted in: ");
-                if (x>Byte.MIN_VALUE && x<=Byte.MAX_VALUE){
-                    System.out.println("* byte");
-                }
-                if (x > Short.MIN_VALUE && x <= Short.MAX_VALUE){
-                    System.out.println("* short");
-                }
-                if (x > Integer.MIN_VALUE && x <= Integer.MAX_VALUE){
-                    System.out.println("* int");
-                }
-                if (x > Long.MIN_VALUE && x <= Long.MAX_VALUE){
-                    System.out.println("* long");
-                }
+//                if (x > Byte.MIN_VALUE && x <= Byte.MAX_VALUE)System.out.println("* byte");
+//                if (x > Short.MIN_VALUE && x <= Short.MAX_VALUE)System.out.println("* short");
+//                if (x > Integer.MIN_VALUE && x <= Integer.MAX_VALUE)System.out.println("* int");
+//                if (x > Long.MIN_VALUE && x <= Long.MAX_VALUE)System.out.println("* long");
+                if(x == (byte)x)System.out.println("* byte");
+                if(x == (short)x)System.out.println("* short");
+                if(x == (int)x)System.out.println("* int");
+                if(x == (long)x)System.out.println("* long");
 
             } catch(Exception e) {
                 System.out.println(sc.next()+" can't be fitted anywhere.");
