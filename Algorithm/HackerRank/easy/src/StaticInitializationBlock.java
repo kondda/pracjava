95e42db8d6f7702e000a97d5da62064e5b1ecfa4
kondda <konddalee@gmail.com>
2021-12-10 20:09:06 +0900

    static, try-catch, initialization block공부

---
+++ Algorithm/HackerRank/easy/src/StaticInitializationBlock.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class StaticInitializationBlock {
+
+    static Scanner sc = new Scanner(System.in);
+    static int B;
+    static int H;
+    static boolean flag = true;
+
+    static{
+        B = sc.nextInt();
+        H = sc.nextInt();
+        sc.close();
+        try {
+            if(B  <= 0 || H <= 0){
+                flag = false;
+                throw new Exception("Breadth and height must be positive");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        if(flag){
+            int area = B * H;
+            System.out.println(area);
+        }
+    }
+}
